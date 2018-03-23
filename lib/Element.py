d5d1855dd2d6c56947d2b635c3493b443ca538ef
gabsmoreira <gabrielfm2@al.insper.edu.br>
2018-03-23 09:26:49 -0300

    element matrix done

--- lib/Element.py
+++ lib/Element.py
@@ -1,14 +1,38 @@
+import sys
+sys.path.insert(0, './lib')
+from Matrix import *
+
 class Element:
-    def __init__(self, element_id, incidence, l, area, c, s):
+    def __init__(self, element_id, incidence, l, area, c, s, e):
         self.element_id = element_id
         self.incidence = incidence
         self.lenght = l
+        self.area = area
         self.cos = c
         self.sin = s
+        self.e = e
+        self.rigid = self.computeRigid()
+        
     
     def console(self):
         print("ELEMENT_ID: {0}".format(self.element_id))
         print("INCIDENCE: {0}".format(self.incidence))
         print("LENGHT: {0}".format(self.lenght))
+        print("AREA: {0}".format(self.area))
         print("COS: {0}".format(self.cos))
         print("SIN: {0}".format(self.sin))
+        print("E: {0}".format(self.e))
+        print("RIGID:")
+        self.rigid.console()
+     
+    
+
+    def computeRigid(self):
+        c = self.cos
+        s = self.sin
+        matrix = [[c**2, c*s, -(c**2), -c*s],
+                  [c*s, s**2, -(c*s), -(s**2)],
+                  [-(c**2), -(c*s), c**2, c*s],
+                  [-(c*s), -(s**2), c*s, s**2]]
+        m = Matrix.arrayToMatrix(matrix)
+        return Matrix.s_multiply(m,(self.e * self.area)/self.lenght)
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import sys
 import math
-# insert path to libs
 sys.path.insert(0, './lib')
 from Matrix import *
 from File import File
@@ -11,18 +10,9 @@ def distance(ax, ay, bx, by):
     return( math.sqrt(pow(bx - ax, 2) + pow(by - ay, 2)))
 
 
+    
 
 a = File("in_test.txt")
-# print("COORDENADAS")
-# print(a.coordinates)
-# print("ELEMENT GROUPS")
-# print(a.element_groups)
-# print("INCIDENCES")
-# print(a.incidences)
-# print(a.bc_nodes)
-# print(a.materials)
-# print(a.geometric_properties)
-# print(a.loads)
 
 list_of_elements = []
 for i in range (len(a.element_groups)):
@@ -35,18 +25,13 @@ for i in range (len(a.element_groups)):
     # print("ax: " + str(ax) + " ay: " + str(ay) + " bx: " + str(bx) + " by: " + str(by))
     l = distance(ax, ay, bx, by)
     # print(l)
-    area = a.geometric_properties[i]
+    e = a.materials[i][0]
+    area = a.geometric_properties[i][0]
     cos = math.fabs(bx - ax)/l
     sin = math.fabs(by - ay)/l
-    element = Element(element_id, incidence, l, area, cos, sin)
+    element = Element(element_id, incidence, l, area, cos, sin, e)
     list_of_elements.append(element)
 
 for j in range (len(list_of_elements)):
     list_of_elements[j].console()
     print("")
-
-# lists = [[4,7], [2,6]]
-# b = toMatrix(lists)
-# i = Matrix.inverse(b)
-# i.console()
-
