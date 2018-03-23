beaa03bdaf57a342a24e9cba5d957aa124862f94
gabsmoreira <gabrielfm2@al.insper.edu.br>
2018-03-23 08:55:43 -0300

    finished element class

--- lib/Element.py
+++ lib/Element.py
@@ -1,8 +1,14 @@
 class Element:
-    def __init__(self, element_id, incidence, l, area, c, s, material):
+    def __init__(self, element_id, incidence, l, area, c, s):
         self.element_id = element_id
         self.incidence = incidence
         self.lenght = l
         self.cos = c
         self.sin = s
-        self.material = material
+    
+    def console(self):
+        print("ELEMENT_ID: {0}".format(self.element_id))
+        print("INCIDENCE: {0}".format(self.incidence))
+        print("LENGHT: {0}".format(self.lenght))
+        print("COS: {0}".format(self.cos))
+        print("SIN: {0}".format(self.sin))
--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import sys
+import math
 # insert path to libs
 sys.path.insert(0, './lib')
 from Matrix import *
 from File import File
 from Element import Element
 
+
+def distance(ax, ay, bx, by):
+    return( math.sqrt(pow(bx - ax, 2) + pow(by - ay, 2)))
+
+
+
 a = File("in_test.txt")
+# print("COORDENADAS")
 # print(a.coordinates)
-print(a.element_groups)
+# print("ELEMENT GROUPS")
+# print(a.element_groups)
+# print("INCIDENCES")
+# print(a.incidences)
 # print(a.bc_nodes)
 # print(a.materials)
 # print(a.geometric_properties)
@@ -15,8 +26,24 @@ print(a.element_groups)
 
 list_of_elements = []
 for i in range (len(a.element_groups)):
-    element_id = a.element_groups[i][0]
-    element = Element()
+    element_id = i
+    incidence = [a.incidences[i][1], a.incidences[i][2]]
+    ax = a.coordinates[incidence[0] - 1][1]
+    ay = a.coordinates[incidence[0] - 1][2]
+    bx = a.coordinates[incidence[1] - 1][1]
+    by = a.coordinates[incidence[1] - 1][2]
+    # print("ax: " + str(ax) + " ay: " + str(ay) + " bx: " + str(bx) + " by: " + str(by))
+    l = distance(ax, ay, bx, by)
+    # print(l)
+    area = a.geometric_properties[i]
+    cos = math.fabs(bx - ax)/l
+    sin = math.fabs(by - ay)/l
+    element = Element(element_id, incidence, l, area, cos, sin)
+    list_of_elements.append(element)
+
+for j in range (len(list_of_elements)):
+    list_of_elements[j].console()
+    print("")
 
 # lists = [[4,7], [2,6]]
 # b = toMatrix(lists)
