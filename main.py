0cc700c4e98f474ddf9cfff2121c890b6714741a
gabsmoreira <gabrielfm2@al.insper.edu.br>
2018-03-23 08:10:55 -0300

    element

--- main.py
+++ main.py
@@ -1,25 +1,24 @@
-
 import sys
-
 # insert path to libs
 sys.path.insert(0, './lib')
 from Matrix import *
 from File import File
+from Element import Element
 
-# a = File("in_test.txt")
+a = File("in_test.txt")
 # print(a.coordinates)
-# print(a.element_groups)
+print(a.element_groups)
 # print(a.bc_nodes)
 # print(a.materials)
 # print(a.geometric_properties)
 # print(a.loads)
- 
+
+list_of_elements = []
+for i in range (len(a.element_groups)):
+    element = Element()
+
 # lists = [[4,7], [2,6]]
 # b = toMatrix(lists)
 # i = Matrix.inverse(b)
 # i.console()
 
-
-a = Matrix.convert(array)
-
-a.console()
