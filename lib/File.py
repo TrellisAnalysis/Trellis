8230c303e013a5be3d5f210024ad0b137deb39e8
gabsmoreira <gabrielfm2@al.insper.edu.br>
2018-03-22 22:29:23 -0300

    fileUpdate

--- lib/File.py
+++ lib/File.py
@@ -2,14 +2,9 @@ class File:
 
     def __init__(self, path):
         self.path = path
-        self.number_of_nodes = None
         self.number_of_element_groups = None
-        self.number_of_materials = None
-        self.number_of_geometric_properties = None
-        self.number_of_bcnodes = None
-        self.number_of_loads = None
         self.coordinates = self.getInfo("COORDINATES")
-        self.element_groups = self.getInfo("ELEMENT_GROUPS")
+        self.element_groups = self.getElement_groups()
         self.incidences = self.getIncidences() # todo: better function
         self.materials = self.getInfo("MATERIALS")
         self.geometric_properties = self.getInfo("GEOMETRIC_PROPERTIES")
@@ -18,7 +13,6 @@ class File:
 
 
     def getInfo(self, info):
-        
         f = open(self.path, 'r')
         line = f.readline()
         while (("*" + info) not in line):
@@ -26,28 +20,16 @@ class File:
         iterator = int(f.readline().split(' ')[0])
         info_list = []
         if(info == "ELEMENT_GROUPS"):
-            print("entrou")
             for i in range(iterator):
                 info_list.append([x for x in f.readline().split()])
             for k in range(2):
                 print(info_list)
                 info_list[i][k] = int(info_list[i][k])
 
-        else:
+        else: 
             for i in range(iterator):
                 info_list.append([float(x) for x in f.readline().split()])
         return info_list
-
-    def getCoordinates(self):
-        f = open(self.path, 'r')
-        linha = f.readline()
-        while ("*COORDINATES" not in linha):
-            linha = f.readline()
-        self.number_of_nodes = int(f.readline().split(' ')[0])
-        coordinates = []
-        for i in range(self.number_of_nodes):
-            coordinates.append([float(x) for x in f.readline().split()])
-        return coordinates
 
     def getElement_groups(self):
         f = open(self.path, 'r')
@@ -72,47 +54,3 @@ class File:
         for i in range(self.number_of_element_groups):
             incidences.append([int(x) for x in f.readline().split()])
         return incidences
-
-    def getMaterials(self):
-        f = open(self.path, 'r')
-        linha = f.readline()
-        while ("*MATERIALS" not in linha):
-            linha = f.readline()
-        self.number_of_materials = int(f.readline().split(' ')[0])
-        materials = []
-        for i in range(self.number_of_materials):
-            materials.append([float(x) for x in f.readline().split()])
-        return materials
-    
-    def getGeometric_properties(self):
-        f = open(self.path, 'r')
-        linha = f.readline()
-        while ("*GEOMETRIC_PROPERTIES" not in linha):
-            linha = f.readline()
-        self.number_of_geometric_properties = int(f.readline().split(' ')[0])
-        geometric_properties = []
-        for i in range(self.number_of_geometric_properties):
-            geometric_properties.append([float(x) for x in f.readline().split()])
-        return geometric_properties
-    
-    def getBC_nodes(self):
-        f = open(self.path, 'r')
-        linha = f.readline()
-        while ("*BCNODES" not in linha):
-            linha = f.readline()
-        self.number_of_bcnodes = int(f.readline().split(' ')[0])
-        bc_nodes = []
-        for i in range(self.number_of_bcnodes):
-            bc_nodes.append([float(x) for x in f.readline().split()])
-        return bc_nodes
-
-    def getLoads(self):
-        f = open(self.path, 'r')
-        linha = f.readline()
-        while ("*LOADS" not in linha):
-            linha = f.readline()
-        self.number_of_loads = int(f.readline().split(' ')[0])
-        loads = []
-        for i in range(self.number_of_loads):
-            loads.append([float(x) for x in f.readline().split()])
-        return loads
--- main.py
+++ main.py
@@ -7,4 +7,10 @@ from Matrix import *
 from File import File
 
 a = File("in_test.txt")
+print(a.coordinates)
 print(a.element_groups)
+print(a.bc_nodes)
+print(a.materials)
+print(a.geometric_properties)
+print(a.loads)
+ 
