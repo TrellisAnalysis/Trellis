6705fdd9b672d863dcdae13f2e129371e3779671
gabsmoreira <gabrielfm2@al.insper.edu.br>
2018-03-29 13:00:42 -0300

    comment

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ sys.path.insert(0, './lib')
 from Matrix import *
 from File import File
 from Element import Element
-
+#top
 def distance(ax, ay, bx, by):
     return( math.sqrt(pow(bx - ax, 2) + pow(by - ay, 2)))
 
