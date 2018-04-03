7d7fc74920401200b3ac4de669ceb676d3d56a67
Filipe Borba <filipefborba@gmail.com>
2018-04-03 17:46:19 -0300

    outputfile, run with command line args, main.py fixes

--- lib/Element.py
+++ lib/Element.py
@@ -2,23 +2,23 @@ import sys
 sys.path.insert(0, './lib')
 from Matrix import *
 
+
 class Element:
     def __init__(self, element_id, incidence, l, area, c, s, e, dof):
         self.element_id = element_id
         self.incidence = incidence
-        self.lenght = l
+        self.length = l
         self.area = area
         self.cos = c
         self.sin = s
         self.e = e
         self.rigid = self.computeRigid()
         self.dof = dof
-        
-    
+
     def console(self):
         print("ELEMENT_ID: {0}".format(self.element_id))
         print("INCIDENCE: {0}".format(self.incidence))
-        print("LENGHT: {0}".format(self.lenght))
+        print("LENGHT: {0}".format(self.length))
         print("AREA: {0}".format(self.area))
         print("COS: {0}".format(self.cos))
         print("SIN: {0}".format(self.sin))
@@ -26,8 +26,6 @@ class Element:
         print("DOF: {0}".format(self.dof))
         print("RIGID:")
         self.rigid.console()
-     
-    
 
     def computeRigid(self):
         c = self.cos
@@ -37,4 +35,6 @@ class Element:
                   [-(c**2), -(c*s), c**2, c*s],
                   [-(c*s), -(s**2), c*s, s**2]]
         m = Matrix.arrayToMatrix(matrix)
-        return Matrix.s_multiply(m,(self.e * self.area)/self.lenght)
+        return Matrix.s_multiply(m, (self.e * self.area) / self.length)
+    
+    
--- main.py
+++ main.py
@@ -1,42 +1,74 @@
-import sys
+import sys, getopt
 import math
 sys.path.insert(0, './lib')
 from Matrix import *
-from File import File
+from File import FileIn, FileOut
 from Element import Element
+
+
 #top
 def distance(ax, ay, bx, by):
-    return( math.sqrt(pow(bx - ax, 2) + pow(by - ay, 2)))
-
-a = File("in_test.txt")
-list_of_elements = []
-for i in range (len(a.element_groups)):
-    element_id = i
-    incidence = [a.incidences[i][1], a.incidences[i][2]]
-    ax = a.coordinates[incidence[0] - 1][1]
-    ay = a.coordinates[incidence[0] - 1][2]
-    bx = a.coordinates[incidence[1] - 1][1]
-    by = a.coordinates[incidence[1] - 1][2]
-    # print("ax: " + str(ax) + " ay: " + str(ay) + " bx: " + str(bx) + " by: " + str(by))
-    l = distance(ax, ay, bx, by)
-    # print(l)
-    e = a.materials[i][0]
-    area = a.geometric_properties[i][0]
-    cos = math.fabs(bx - ax)/l
-    sin = math.fabs(by - ay)/l
-    dof = []
-    for j in range(len(a.bc_nodes)):
-        if(a.bc_nodes[j][0] in incidence):
-            dof.append(j+1) 
-       
-        
-    element = Element(element_id, incidence, l, area, cos, sin, e, dof)
-    list_of_elements.append(element)
+    return (math.sqrt(pow(bx - ax, 2) + pow(by - ay, 2)))
 
 
-## print info
-# for j in range (len(list_of_elements)):
-#     list_of_elements[j].console()
-#     print("")
+def load_file(inputfile):
+    a = FileIn(inputfile)
+    list_of_elements = []
+    for i in range(len(a.element_groups)):
+        element_id = i
+        incidence = [a.incidences[i][1], a.incidences[i][2]]
+        ax = a.coordinates[incidence[0] - 1][1]
+        ay = a.coordinates[incidence[0] - 1][2]
+        bx = a.coordinates[incidence[1] - 1][1]
+        by = a.coordinates[incidence[1] - 1][2]
+        # print("ax: " + str(ax) + " ay: " + str(ay) + " bx: " + str(bx) + " by: " + str(by))
+        l = distance(ax, ay, bx, by)
+        # print(l)
+        e = a.materials[i][0]
+        area = a.geometric_properties[i][0]
+        cos = math.fabs(bx - ax) / l
+        sin = math.fabs(by - ay) / l
+        dof = []
+        for j in range(len(a.bc_nodes)):
+            if (a.bc_nodes[j][0] in incidence):
+                dof.append(j + 1)
+
+        element = Element(element_id, incidence, l, area, cos, sin, e, dof)
+        list_of_elements.append(element)
+
+    ## print info
+    # for j in range (len(list_of_elements)):
+        #     list_of_elements[j].console()
+        #     print("")
 
 
+def main(argv):
+    inputfile = ''
+    outputfile = ''
+    try:
+        opts, args = getopt.getopt(argv, "hi:o:", ["ifile=", "ofile="])
+    except getopt.GetoptError:
+        print('Usage is: test.py -i <inputfile> -o <outputfile> or test.py -h for help')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('test.py -i <inputfile> -o <outputfile>')
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            inputfile = arg
+        elif opt in ("-o", "--ofile"):
+            outputfile = arg
+        else:
+            print('An error ocurred, type "test.py -h for help"')
+            sys.exit()
+
+    print('Input file is:', inputfile)
+    print('Output file is:', outputfile)
+
+    load_file(inputfile)
+
+    output = FileOut(outputfile)
+    output.writeOutputFile()
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
